7f45a02d84a01c50c1c3ffca61725b1672d5642e
XIwE1 <804393870@qq.com>
2024-05-31 02:39:03 +0800

    perf:补充start的demo

--- document/start/index.js
+++ document/start/index.js
@@ -11,6 +11,7 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
